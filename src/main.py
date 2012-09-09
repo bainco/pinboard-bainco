1f99c139f43861d2a3283ce6ac999bcbe9b189fb
bainco <bainco@email.sc.edu>
2012-09-09 11:46:14 -0400

    Commit for HW 2. Removes unnecessary files from HW 1. Adds basic jinja2 templating (base.html and pinboard.html) as well as pinning functionality through a GET form. Also adds a README.md file.

---
+++ src/main.py
@@ -0,0 +1,28 @@
+#Author: Connor P. Bain
+#HW 2
+#Added jinja2 templates as well as simple pinning ability.
+#Last modified September 7, 2012
+
+import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
+
+class MainPage(webapp2.RequestHandler):        
+    def get(self):
+        imgUrl = self.request.get('imgUrl')
+        caption = self.request.get('caption')
+                
+        template_values = {
+            'title': 'Homework 2',
+            'imgUrl': imgUrl,
+            'caption': caption
+            }
+        
+        template = jinja_environment.get_template('pinboard.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
