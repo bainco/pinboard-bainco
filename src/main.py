96dcb0e24aed0dceb7f8ec69cafeff897ad961c6
bainco <bainco@email.sc.edu>
2012-09-14 12:34:26 -0400

    Commit for HW 3. Implements Users API for simple login/logout functionality.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,32 @@
 #Author: Connor P. Bain
 #HW 3
-#Added jinja2 templates as well as simple pinning ability.
-#Last modified September 7, 2012
+#Added CSS styling and implemented Users API
+#Last modified September 14, 2012
 
 import webapp2
 import jinja2
 import os
+from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 class MainPage(webapp2.RequestHandler):        
     def get(self):
+        currentUser = users.get_current_user()
         imgUrl = self.request.get('imgUrl')
         caption = self.request.get('caption')
-                
+        
         template_values = {
             'title': 'Pinboard',
             'imgUrl': imgUrl,
-            'caption': caption
+            'caption': caption,
+            'user': currentUser,
+            'login': users.create_login_url(self.request.uri),
+            'logout': users.create_logout_url("/")
             }
-        
-        template = jinja_environment.get_template('pinboard.html')
+
+        template = jinja_environment.get_template('pinboard.html')   
         self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
