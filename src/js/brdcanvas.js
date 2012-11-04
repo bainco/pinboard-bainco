f551c7ac38d65d29bd68a784637d187a8b4a1a6b
bainco <bainco@email.sc.edu>
2012-11-04 12:03:37 -0500

    Added pin dragging on the canvas.

--- src/js/brdcanvas.js
+++ src/js/brdcanvas.js
@@ -1,11 +1,14 @@
-var theCanvas;
-var ctx;
-var boardID;
-var theBoard;
 var PIN_WIDTH = 200;
 var PIN_HEIGHT = 200;
 var CANVAS_HEIGHT = 480;
 var CANVAS_WIDTH = 854;
+
+var theCanvas;
+var ctx;
+var boardID;
+var theBoard;
+var dragPin;
+var thePinImages = new Array();
 
 $(document).ready(function() {
 	console.log("ready");
@@ -39,36 +42,87 @@ function setupCanvas() {
 	ctx = theCanvas.getContext('2d');
 	theCanvas.addEventListener('mousedown', handleMouseDown);
 	theCanvas.addEventListener('mouseup', handleMouseUp)
-	
+
 	var loopPin;
 	for (var i = 0; i < theBoard.pins.length; i++) {
-		
+
+		theBoard.pins[i].x = (i * 200)
+		theBoard.pins[i].y = 0;
 		loopPin = theBoard.pins[i];
 		drawPin(loopPin);
 	}
 }
 
 function drawPin(thePin) {
+
 	var img = new Image();
 	img.onload = function(){
-		ctx.drawImage(img, 0, 0, PIN_WIDTH, PIN_HEIGHT);
+		ctx.drawImage(img, thePin.x, thePin.y, PIN_WIDTH, PIN_HEIGHT);
 	};
 	img.src = thePin.imgUrl;
-	img.alt = thePin.id;
+	img.setAttribute("pinid", thePin.id);
+	img.setAttribute("x", thePin.x);
+	img.setAttribute("y", thePin.y);
+	thePinImages.push(img);
 }
 
 function handleMouseDown(event) {
 
 	console.log("Mouse down.");
-	theCanvas.addEventListener('mousemove', handleDrag);
+	if (validClick(event.offsetX, event.offsetY) == true) {
+		theCanvas.addEventListener('mousemove', handleDrag);
+	}
 }
 
+function validClick(mouseX, mouseY) {
+
+	var loopPin;
+	var loadX;
+	var loadY;
+
+	for (var i = 0; i < thePinImages.length; i++) {
+
+		loopPin = thePinImages[i];
+		loadX = eval(thePinImages[i].getAttribute('x'));
+		loadY = eval(thePinImages[i].getAttribute('y'));
+
+		if ((mouseX < ((loadX + 200))) && (mouseX > loadX)) {
+			if ((mouseY > loadY) && (mouseY < (loadY + 200))) {
+				console.log("Valid click");
+				dragPin = loopPin;
+				return true;
+			}
+		}
+	}
+	return false;
+}
 function handleMouseUp(event) {
 	console.log("Mouse released.");
+	//STUB: send to server
+	dragPin = null;
 	theCanvas.removeEventListener('mousemove', handleDrag);
 }
 
 function handleDrag(event) {
-	
+
 	console.log("Dragging!");
+	ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	var loopImage;
+	for (var i = 0; i < thePinImages.length; i++) {
+		if (dragPin.getAttribute("pinid") == thePinImages[i].getAttribute("pinid")) {
+			thePinImages[i].setAttribute('x', event.offsetX);
+			thePinImages[i].setAttribute('y', event.offsetY);;
+		}
+	}
+	updateCanvas();
+}
+
+function updateCanvas() {
+
+	var loopImage;
+	for (var i = 0; i < thePinImages.length; i++) {
+
+		loopImage = thePinImages[i];
+		ctx.drawImage(loopImage, loopImage.getAttribute('x'), loopImage.getAttribute('y'), PIN_WIDTH, PIN_HEIGHT);
+	}
 }
